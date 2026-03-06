791e33840ea6345946a19e86c52103839ae2b2ea
ShanzayHussain <shanzayhussain21212@gmail.com>
2026-03-07 00:26:17 +0500

    login/Register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import { mockData } from '../mockData';
+import logo from "../assets/logo.png";
 
 function Login() {
     return (
-        <div className="page form-container">
-            <h2>Login to SSCP</h2>
+        <div className=" page form-container">
+            <img className="image" src={logo} alt="" />
+            <h1 className="heading">Sindh Smart Citizen Portal</h1>
+            <h2 className="heading">Login</h2>
+            
             <form onSubmit={(e) => e.preventDefault()}>
                 <div className="form-group">
-                    <label>Email Address</label>
-                    <input type="email" defaultValue={mockData.currentUser.email} />
+                    <label>CNIC Number</label>
+                        <input
+                        type="text"
+                        maxLength="15"
+                        pattern="[0-9]{5}-[0-9]{7}-[0-9]{1}"
+                        placeholder="XXXXX-XXXXXXX-X"
+                        defaultValue={mockData.currentUser.cnic}
+                        required
+                        />
                 </div>
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" defaultValue="password123" />
+                    <input type="password" placeholder="********" />
                 </div>
                 <button type="submit">Login</button>
             </form>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,30 +1,57 @@
 import React from 'react';
 import { mockData } from '../mockData';
+import logo from "../assets/logo.png";
 
 function Register() {
     const user = mockData.currentUser;
 
     return (
         <div className="page form-container">
-            <h2>Create Citizen Account</h2>
+            <img className="image" src={logo} alt="" />
+                        <h1 className="heading">Sindh Smart Citizen Portal</h1>
+                        <h2 className="heading">Create Account</h2>
             <form onSubmit={(e) => e.preventDefault()}>
                 <div className="form-group">
                     <label>Full Name</label>
-                    <input type="text" defaultValue={user.name} />
+                    <input type="text" placeholder='e.g. Jane Doe' />
                 </div>
                 <div className="form-group">
                     <label>Email Address</label>
-                    <input type="email" defaultValue={user.email} />
+                    <input type="email" placeholder='e.g. janedoe@gmail.com' />
                 </div>
+                 <div className="form-group">
+                                    <label>CNIC Number</label>
+                                        <input
+                                        type="text"
+                                        maxLength="15"
+                                        pattern="[0-9]{5}-[0-9]{7}-[0-9]{1}"
+                                        placeholder="XXXXX-XXXXXXX-X"
+                                        
+                                        />
+                                </div>
                 <div className="form-group">
                     <label>Phone Number</label>
-                    <input type="text" defaultValue={user.phone} />
+                    <input type="text" placeholder='+92' />
                 </div>
                 <div className="form-group">
                     <label>Address</label>
-                    <textarea defaultValue={user.address}></textarea>
+                    <textarea placeholder='Enter your residential address'></textarea>
                 </div>
-                <button type="submit">Register</button>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" placeholder="Enter your password" />
+                </div>
+                <div className="form-group">
+                    <label>Confirm Password</label>
+                    <input type="password" placeholder="Re-enter password" />
+                    </div>
+                    <div className="form-group terms">
+                                <input type="checkbox" id="terms" />
+                                <label htmlFor="terms">
+                                    I agree to the <a href="/terms">terms and conditions</a>
+                                </label>
+                                </div>
+                    <button type="submit">Register</button>
             </form>
         </div>
     );
